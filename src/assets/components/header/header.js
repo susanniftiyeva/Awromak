cbfe86886ccb670ba72b1ed023b52fc9c6f272f7
OrxanMukhtar <a.muxtarli92@gmail.com>
2024-01-26 22:16:39 +0400

    header

--- src/assets/components/header/header.js
+++ src/assets/components/header/header.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logo from '../../img/icon/header/Frame.png'
 
 function Header() {
   return (
-    <div>
-
+    <div className='header_body container'>
+        <div>
+            <img src={logo} alt='logo'/>
+        </div>
         <ul className='menu'>
             <li>
                 <Link to="/" >Home</Link>
