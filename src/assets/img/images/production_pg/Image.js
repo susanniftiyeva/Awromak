7577c0ed062a4e34231689a776981750863115da
MineverSadigova <minevver2003@gmail.com>
2024-01-27 14:54:49 +0400

    istehsalat

---
+++ src/assets/img/images/production_pg/Image.js
@@ -0,0 +1,26 @@
+import image82 from './image82.png'
+import image83 from './image83.png'
+import image84 from './image84.png'
+import image85 from './image85.png'
+import image88 from './image88.png'
+import image89 from './image89.png'
+import image90 from './image90.png'
+import image91 from './image91.png'
+import pasta1 from './pasta1.png'
+import pasta2 from './pasta2.png'
+import pasta3 from './pasta3.png'
+let Image={
+    image82,
+    image83,
+    image84,
+    image85,
+    image88,
+    image89,
+    image90,
+    image91,
+    pasta1,
+    pasta2,
+    pasta3
+}
+
+export default Image
--- src/assets/package/production/production.js
+++ src/assets/package/production/production.js
@@ -1,17 +1,57 @@
-import React from 'react'
-import '../../css/production_pg/production.scss'
-
+import React from "react";
+import "../../css/production_pg/production.scss";
+import Image from "../../img/images/production_pg/Image";
 const Production = () => {
   return (
-    <div className='container'>
-        <div className='section'>
-            <h1 className='border-bottom'>Avromak şirketi yarandigi gunden keyfiyyetli mehsul ve
-                 mehsullarin vaxtinda unvana chatdirilma prinsipleri esasinda hem yerli
-                  hem de xarici bazarda genish nufuz qazanmishdir.</h1>
-
+    <div className="container">
+      <div className="section">
+        <h1 className="border-bottom">
+          Avromak şirketi yarandigi gunden keyfiyyetli mehsul ve mehsullarin
+          vaxtinda unvana chatdirilma prinsipleri esasinda hem yerli hem de
+          xarici bazarda genish nufuz qazanmishdir.
+        </h1>
+        <img className="img1"  src={Image.image82} />
+        <p>
+          Avromak MMC olaraq, qurulduğumuz gündən gücünü torpaqdan alan kənd
+          təsərrüfatı məhsullarımızla fəaliyyət göstəririk. İnvestisiyalarımızı
+          edərkən, biz bunu ekoloji cəhətdən davamlı, sosial cəhətdən
+          məsuliyyətli və iqtisadi artımı dəstəkləyən bir strategiya ilə edirik.
+          Sahelerimizden elde olunan bugda mehsullari Fabrikimizde uyudulerek
+          onlardan Un ve Makaron yeyinti mehsullari elde olunur. Un ve Makaron
+          Mehsularimizin erseye getirilmesinde yuksek ixtisasli
+          emekdashlarimizin boyuk emeyi vardir. Istehsalat avadanliqlari
+          butunlukle Italiya istehsalidir. Qloballaşan kənd təsərrüfatı siyasəti
+          taxıl istehsalına marağı artırır və kənd təsərrüfatı istehsalında
+          monokulturaya önəm verir. İstehsalın dəyişməsi ilə yanaşı, gəlir
+          səviyyəsinin dəyişməsi və sürətli urbanizasiya kənd təsərrüfatı
+          məhsullarına olan tələbata qidalanma vərdişlərindən tutmuş ərzaq
+          təminatına qədər fərqli ölçülər əlavə edir.
+        </p>
+        <div className="parent">
+          <div className="hektar">
+          <span >
+          12000 ha  
+        </span>  
+        <span >
+        2500 m<sup>2</sup>
+        </span>
+          </div>
+          <hr/>
+          
+          <div className="sahe">
+          <span >
+          ƏKİN SAHƏSİ
+        </span>  
+        <span >
+          İSTEHSAL SAHƏSİ
+        </span>
+          </div>
         </div>
-    </div>
-  )
-}
+      </div>
 
-export default Production
+
+    </div>
+  );
+};
+
+export default Production;
