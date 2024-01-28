d9f975ddd7af2675953ec3c91aeea0f12e58238d
OrxanMukhtar <a.muxtarli92@gmail.com>
2024-01-28 12:42:21 +0400

    home pg

--- src/assets/package/home_pg/home.js
+++ src/assets/package/home_pg/home.js
@@ -2,9 +2,16 @@ import React from 'react'
 import '../../css/home_pg/home.css'
 import '../../css/home_pg/home.scss'
 
+import right_arc from '../../img/images/home_pg/icon/to_right_icon.svg'
+
 function Home() {
   return (
-    <h1>home</h1>
+    <div className='home_body container'>
+      <header className='header_content'>
+          <h1>Zövqü və ruhu zənginləşdirən keyfiyyətli məhsul hazırlamaqla uğur qazanırıq</h1>
+          <img src={right_arc} />
+      </header>
+    </div>
   )
 }
 
