087cb6d8e42497ebc81fddf8919a74a40909169a
MineverSadigova <minevver2003@gmail.com>
2024-02-07 16:05:04 +0400

    responsive

--- src/assets/package/meat.pg/meat.js
+++ src/assets/package/meat.pg/meat.js
@@ -9,6 +9,7 @@ const Meat = () => {
         <div className="article-mt1">
           <div>
             <h3>“AVROMAK MMC” şirkətinin ət məhsulları istehsalatı</h3>
+            <div className="mobileImg"><img src={Image_meat.image777} /></div>
             <p>
               Kolbasa İstehsal bolumunde Istehsal tam olaraq avtomatlaşdırılıb
               və prosesə daxil olan avadanlıqlarımız Almaniya, İtaliya və
@@ -19,21 +20,31 @@ const Meat = () => {
               keyfiyyətdə məhsul ərsəyə gətirməkdir.
             </p>
           </div>
-          <img src={Image_meat.image777} />
+        <div className="desktopImg">  <img src={Image_meat.image777} /></div>
         </div>
 
         <div className="article-mt2">
           <h3>Ət məhsulları</h3>
           <div className="production">
+           <div> 
             <img src={Image_meat.image778} />
-            <img src={Image_meat.image778} />
-            <img src={Image_meat.image778} />
-          </div>
-
-          <div className="production-name">
+            <hr/>
             <p>Dadım</p>
+            <hr/>
+            </div>
+            <div> 
+            <img src={Image_meat.image779} />
+            <hr/>
             <p>Üstdad</p>
+            <hr/>
+            </div>
+            <div> 
+            <img src={Image_meat.image780} />
+            <hr/>
             <p>Rosko</p>
+            <hr/>
+            </div>
+          
           </div>
         </div>
       </div>
