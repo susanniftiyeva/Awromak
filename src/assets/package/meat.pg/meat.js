83e6029f97fe9a49abf399e6fac51f7c23c50ce8
OrxanMukhtar <a.muxtarli92@gmail.com>
2024-02-10 21:55:47 +0400

    axios meat

--- src/assets/package/meat.pg/meat.js
+++ src/assets/package/meat.pg/meat.js
@@ -1,50 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../css/meat_pg/meat.scss";
 import "../../img/images/meat_pg/Image_meat";
 import Image_meat from "../../img/images/meat_pg/Image_meat";
+import axios from "axios";
+
 const Meat = () => {
+
+  const [meatImg, setMeatImg] = useState();
+
+  axios.get("https://dummyjson.com/products/1")
+  .then((resp) => {
+    setMeatImg(resp.data);
+
+  });
+  // console.log(meatImg)
   return (
     <div className="custon_container">
       <div className="meat-section">
         <div className="article-mt1">
           <div>
             <h3>“AVROMAK MMC” şirkətinin ət məhsulları istehsalatı</h3>
-            <div className="mobileImg"><img src={Image_meat.image777} /></div>
-            <p>
-              Kolbasa İstehsal bolumunde Istehsal tam olaraq avtomatlaşdırılıb
-              və prosesə daxil olan avadanlıqlarımız Almaniya, İtaliya və
-              Polşadan gətirilib. Fəaliyyətimiz halallıq, düzgünlük, ekoloji
-              təbiilik, daimi inkişaf, şəffaflıq və məsuliyyətlilik prinsipləri
-              üzərində qurulub. Əsas fəlsəfəmiz isə alıcıların istəklərini
-              diqqətlə dəyərləndirmək və bu istəklərə uyğun ən yüksək
-              keyfiyyətdə məhsul ərsəyə gətirməkdir.
-            </p>
+            <div className="mobileImg">
+              <img src={meatImg?.thumbnail} />
+            </div>
+              <p>{meatImg?.description}</p>
           </div>
-        <div className="desktopImg">  <img src={Image_meat.image777} /></div>
+          <div className="desktopImg">
+            {" "}
+            <img src={meatImg?.thumbnail} />
+          </div>
         </div>
 
         <div className="article-mt2">
-          <h3>Ət məhsulları</h3>
-          <div className="production">
-           <div> 
-            <img src={Image_meat.image778} />
-            <hr/>
-            <p>Dadım</p>
-            <hr/>
-            </div>
-            <div> 
-            <img src={Image_meat.image779} />
-            <hr/>
-            <p>Üstdad</p>
-            <hr/>
-            </div>
-            <div> 
-            <img src={Image_meat.image780} />
-            <hr/>
-            <p>Rosko</p>
-            <hr/>
-            </div>
           
+          <h3 key={''}>{meatImg?.brand}</h3>
+          <div className="production">
+     
+
+            {meatImg?.images.map(image => {
+              return (
+                <div>
+                  <img src={image} />
+                  <hr />
+                  <p>Dadım</p>
+                  <hr />
+                </div>
+              );
+            })}
+
           </div>
         </div>
       </div>
